af8b4b0223426b44b95523a4f0fa6714146ed231
mohskhan <mohskhan@bmc.com>
2018-01-30 17:28:13 +0530

    0.1

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const http = require('http');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+const mongoose = require('mongoose');
+const router = require('./router');
+
+var app = express();
+
+// DB Setup
+mongoose.connect('mongodb://127.0.0.1/auth', { useMongoClient: true }, function(error) {
+    if(error) { console.error('error: ', error); }
+});
+
+// App Setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type : '*/*' }));
+
+// Append routes to the app
+router(app);
+
+// Server Setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on: ', port);
